e3f3a2a1c8ded048081d3998632277413b85b9c8
mimaraslan <mimaraslan@gmail.com>
2019-05-11 23:57:59 +0300

    java
    java

---
+++ java-se/oca/_01_JavaClassStructure/src/_007_redundant_imports/ImportExample.java
@@ -0,0 +1,13 @@
+package _007_redundant_imports;
+
+import java.lang.System;
+import java.lang.*; //Redundant Import
+import java.util.Random;
+import java.util.*; //Redundant Import
+
+public class ImportExample {
+	public static void main(String[] args) {
+		Random r = new Random();
+		System.out.println(r.nextInt(10));
+	}
+}
