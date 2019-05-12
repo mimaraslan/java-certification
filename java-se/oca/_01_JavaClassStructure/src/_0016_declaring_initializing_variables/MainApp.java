762181bbbd7c2f85b91e9efc3eebeeb35985ce56
mimaraslan <mimaraslan@gmail.com>
2019-05-13 01:41:04 +0300

    Java
    Java

---
+++ java-se/oca/_01_JavaClassStructure/src/_0016_declaring_initializing_variables/MainApp.java
@@ -0,0 +1,21 @@
+package _0016_declaring_initializing_variables;
+
+public class MainApp {
+	public static void main(String[] args) {
+		
+		String zooName1; 
+		int numberAnimals1;
+		
+		zooName1 = "The Best Zoo1"; 
+		numberAnimals1 = 100;
+		
+		String zooName2 = "The Best Zoo2"; 
+		int numberAnimals2 = 200;		
+		
+		System.out.println(zooName1);
+		System.out.println(numberAnimals1);
+
+		System.out.println(zooName2);
+		System.out.println(numberAnimals2);
+	}
+}
