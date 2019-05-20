fbb8fc0c5e1b2c33fa05126e7bdb2b3ee5386f4e
mimaraslan <mimaraslan@gmail.com>
2019-05-20 23:30:46 +0300

    Java
    Java

---
+++ java-se/oca/_01_JavaClassStructure/src/_0017_identifiers/MainApp.java
@@ -0,0 +1,38 @@
+package _0017_identifiers;
+
+public class MainApp {
+	public static void main(String[] args) {
+		
+		int okidentifier; 
+		int $OK2Identifier; 
+		int _alsoOK1d3ntifi3r; 
+		int __SStillOkbutKnotsonice$;
+	
+		/*
+		
+		3DPointClass // identifiers cannot begin with a number 
+
+		adana@merkez // @ is not a letter, digit, $ or _ 
+		
+		*$coffee // * is not a letter, digit, $ or _
+		
+		public // public is a reserved word
+		
+		
+		List of Reserved Java Keywords
+		
+abstract	assert	boolean	break	byte		case
+catch		char	class	const	continue	default
+double		do		else	enum	extends		false
+final		finally	float	for		goto		if
+implements	import	instanceof		int			interface	long
+native		new		null	package	private		protected
+public		return	short	static	strictfp	super
+switch		synchronized	this	throw		throws	transient
+true		try		void	volatile		while
+
+		*/
+			
+		
+	}
+}
