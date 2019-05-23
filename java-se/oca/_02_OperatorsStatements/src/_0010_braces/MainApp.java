c895c263957efc32d1d3d08f45ccd6583621a331
mimaraslan <mimaraslan@gmail.com>
2019-05-24 01:12:09 +0300

    Java
    Java

---
+++ java-se/oca/_02_OperatorsStatements/src/_0010_braces/MainApp.java
@@ -0,0 +1,22 @@
+package _0010_braces;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+		/*
+		boolean x = true == 3; // DOES NOT COMPILE
+		boolean y = false != "Giraffe"; // DOES NOT COMPILE 
+		boolean z = 3 == "Kangaroo"; // DOES NOT COMPILE
+		 */
+		
+		int x = 1;
+		if(x) { 	// DOES NOT COMPILE
+		 }
+		
+		int x = 1;
+		if(x = 5) { // DOES NOT COMPILE
+		} 
+		
+	}
+}
---
+++ java-se/oca/_02_OperatorsStatements/src/_0011_ternary_operator/MainApp.java
@@ -0,0 +1,21 @@
+package _0011_ternary_operator;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+
+		int y = 10;
+		/*
+		final int x;
+		if (y > 5) {
+			x = 2 * y;
+		} else {
+			x = 3 * y;
+		}
+		*/
+		final int x = (y > 5) ? (2 * y) : (3 * y);
+		
+		System.out.println(x);
+
+	}
+}
