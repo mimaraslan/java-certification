a841f2a9709529b35eaa3ee6666293ca4c6acb26
Mimar Aslan <mimaraslan@gmail.com>
2019-05-28 02:02:57 +0300

    Java
    Java

--- java-se/oca/_04_methods_encapsulation/src/_0008_varargs/MainApp.java
+++ java-se/oca/_04_methods_encapsulation/src/_0008_varargs/MainApp.java
@@ -7,18 +7,17 @@ public class MainApp {
 	public void walk4(int... nums, int start) { } // DOES NOT COMPILE
 	public void walk5(int... start, int... nums) { } // DOES NOT COMPILE
 	public void walk6(int start, int nums ...) { } // DOES NOT COMPILE
-
 	
 	public void walk7(int... a, int b, int c ) { } // DOES NOT COMPILE
 	public void walk8(int ... a, int b, int c ) { } // DOES NOT COMPILE
 	public void walk9(int a..., int b, int c ) { } // DOES NOT COMPILE
 	
-	public void walk7(int a, int... b, int c ) { } // DOES NOT COMPILE
-	public void walk8(int a, int ... b, int c ) { } // DOES NOT COMPILE
-	public void walk9(int a, int b ..., int c) { }// DOES NOT COMPILE
+	public void walk10(int a, int... b, int c ) { } // DOES NOT COMPILE
+	public void walk11(int a, int ... b, int c ) { } // DOES NOT COMPILE
+	public void walk12(int a, int b ..., int c) { }// DOES NOT COMPILE
 	
-	public void walk7(int a, int b, int... c ) { } 
-	public void walk8(int a, int b, int ... c ) { } // DOES NOT COMPILE
-	public void walk9(int a, int b, int c ...) { } // DOES NOT COMPILE
+	public void walk13(int a, int b, int... c ) { } 
+	public void walk14(int a, int b, int ... c ) { } // DOES NOT COMPILE
+	public void walk15(int a, int b, int c ...) { } // DOES NOT COMPILE
 
 }
