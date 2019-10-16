7e949bf46724dee6f6c1d92c3b4d76f1741c70af
mimaraslan <mimaraslan@gmail.com>
2019-10-16 21:54:44 +0300

    Java OCA OCP
    Java OCA OCP

--- java-se/ocp/_07_AdvancedClassDesign/src/_012_enums/MainApp.java
+++ java-se/ocp/_07_AdvancedClassDesign/src/_012_enums/MainApp.java
@@ -2,20 +2,32 @@ package _012_enums;
 
 public class MainApp {
 	public static void main(String[] args) {
-		Season s = Season.SUMMER;
+		
+		Season s1 = Season.SUMMER;
+		System.out.println(s1);
+		
+		System.out.println("--------------");
+		
 		System.out.println(Season.SUMMER); // SUMMER
-		System.out.println(s == Season.SUMMER); // true
+		System.out.println(s1 == Season.SUMMER); // true
+
+		System.out.println("--------------");
 
 		for (Season season : Season.values()) {
 			System.out.println(season.name() + " " + season.ordinal());
 		}
-		
-		
+				
 	// enum is a type and not an int.	
 	// if ( Season.SUMMER == 2) {} // DOES NOT COMPILE
 		
+		System.out.println("--------------");
 		
-		Season s1 = Season.valueOf("SUMMER"); // SUMMER
-		Season s2 = Season.valueOf("summer"); // java.lang.IllegalArgumentException
+		Season s2 = Season.valueOf("SUMMER"); // SUMMER
+		System.out.println(s2);
+
+		System.out.println("--------------");
+
+		Season s3 = Season.valueOf("summer"); // java.lang.IllegalArgumentException: No enum constant
+		System.out.println(s3);
 	}
 }
