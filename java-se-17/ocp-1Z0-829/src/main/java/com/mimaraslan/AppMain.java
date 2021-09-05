4ebcadc5f9533e0f7f477f6a6d76080ae8d381c5
mimaraslan <mimaraslan@gmail.com>
2021-09-06 01:24:42 +0300

    Java OCP
    Java OCP

--- java-se-17/ocp-1Z0-829/src/main/java/com/mimaraslan/AppMain.java
+++ java-se-17/ocp-1Z0-829/src/main/java/com/mimaraslan/AppMain.java
@@ -1,2 +1,7 @@
-package com.mimaraslan;public class AppMain {
+package com.mimaraslan;
+
+public class AppMain {
+    public static void main(String[] args) {
+        System.out.println("Hello Java SE 17");
+    }
 }
